28989602d7377c58b14a6366229de3785f29d7f0
dudu200106 <1247187273@qq.com>
2022-03-06 23:32:00 +0800

    Changes

--- src/math_数学/Bezout_Numbers.java
+++ src/math_数学/Bezout_Numbers.java
@@ -22,6 +22,16 @@ public class Bezout_Numbers {
      */
 
     //扩展欧几里得方法，方便下一步求贝组数（返回的是最大公约数）
+
+    /**
+     * 定义了两个变量x,y分别代表使贝祖等式成立时的贝祖数
+     * 裴蜀定理: ax + by = m (m为gcd(a,b)的倍数)
+     * 实际运用: 得知了a,b的gcd, 可以由a,b配出任何gcd的倍数--若gcd(a,b)=1, 可以组合处任何数
+     * 扩展欧几里得在这里的任务: 计算出使ax + by = gcd 成立的x和y -- 若是不成立
+     * @param a
+     * @param b
+     * @return gcd 最小大公约数:greatest common divisor
+     */
     public static long egcb(long a,long b){
         if(b==0) {
             x =1;
@@ -29,22 +39,29 @@ public class Bezout_Numbers {
             return a;
         }
 
-        long temp=egcb(b,a%b);
+        long gcd=egcb(b,a%b);
         long x1=x;
         x=y;
         y=x1-(a/b)*y;
 
-        return temp;
+        return gcd;
     }
 
     //返回最大该公约数，若等式不成立则返回-1
+    /**
+     * ax+by=m (m为gcd(a,b)的倍数)
+     * @param a
+     * @param b
+     * @param m
+     * @return n 最大公约数的倍数
+     */
     public static long bezout_Number(long a, long b, long m){
         long n = m/egcb(a,b); //等式右边的值，最大公约数的n倍
         if(m%egcb(a,b)!=0) {
             System.out.println("m非两数最大公约数倍数，无解！！");
             return -1;
         }
-        x*=n;
+        x*=n;//x,y是静态变量
         y*=n;
 
         return n;
