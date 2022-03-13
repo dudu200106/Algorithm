da8e6f6843f22e3f85bdfe6d2b8507e4bcba1198
dudu200106 <1247187273@qq.com>
2022-03-13 13:38:59 +0800

    changes

--- src/math_数学/FastPower.java
+++ src/math_数学/FastPower.java
@@ -1,20 +1,44 @@
 package math_数学;
 
+/**
+ * 这里介绍了两种快速幂的思想:
+ *   1.二分思想, 不断使基数平方翻倍,指数减半
+ *   2.位运算, 将指数转换成二进制数, 每位二进制位代表此位是否取得到, 然后累乘得到result
+ *最后还可以拓展一下矩阵幂运算的思想--首先得学会斐波那契的矩阵解法
+ */
 public class FastPower {
-    public static int fastPower(int base, int power){
-        int answer=1;
-        while(power>1){
-            if(power%2==1){
-                answer=answer*base;
-                power-=1;
-            }
-            power*=0.5;
-            base*=base;
-            answer=(int)(Math.pow(base,power));
+
+    static int fast1(int base, int power){
+        if (power==0) return 1;
+        if (power==1) return base;
+
+        int res=1;
+        int temp=base;
+        int exponent=1;
+        while((exponent<<1)<=power){
+            temp*=temp;
+            exponent<<=1;
         }
-        return answer;
+        res=temp * fast1(base,power-exponent);
+        return res;
     }
+
+   /*利用power的二进制--如10次幂的二进制格式:1010, 每位对应着(base)^2^n-1要不要取, 由小到大平方翻番累乘上去*/
+   static int fast2(int base, int power){
+       int res=1;
+       int bit=base; //bit代表每位的进位数,现在是最小的那位
+       while (power>0){
+           if ((power&1)==1){ //判断是否取得到此时的最低位
+               res*=bit;
+           }
+           bit*=bit; //无论此二进制位是否取得到,进位都要平方翻番
+           power>>=1; //移位至更高位
+       }
+       return res;
+   }
+
     public static void main(String[] args){
-        System.out.println(fastPower(9,5));
+        System.out.println(fast1(9,5));
+        System.out.println(fast2(9,5));
     }
 }
