81e6709ba6f3eeb0aec74a6b732d21a6dbe6dbde
student <2415080861@qq,com>
2022-04-05 10:58:01 +0800

    review and changes

--- src/graph_图论/图的dfs/Kruskal.java
+++ src/graph_图论/图的dfs/Kruskal.java
@@ -1,23 +1,80 @@
 package graph_图论.图的dfs;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /* 最小生成树*/
 public class Kruskal {
 
     public static Edge[] edges; //边集合
-    public static ArrayList<Edge> list; //存放最小生成树的边
-    public static int[] prev;  //每个顶点的最小权重前驱
-    public static int[] p; //并查集
+    public static ArrayList<Edge> list =new ArrayList<>(); //存放最小生成树的边
+    public static int[] prev =new int[256];  //每个顶点的最小权重前驱
+    public static int[] p =new int[256]; //并查集
 
+    static {
+        edges=new Edge[10];
+        edges[0]=new Edge('C', 'D', 1);
+        edges[1]=new Edge('C', 'A', 1);
+        edges[2]=new Edge('C', 'E', 8);
+        edges[3]=new Edge('A', 'B', 3);
+        edges[4]=new Edge('D', 'E', 3);
+        edges[5]=new Edge('B', 'C', 5);
+        edges[6]=new Edge('B', 'E', 6);
+        edges[7]=new Edge('B', 'D', 7);
+        edges[8]=new Edge('A', 'D', 2);
+        edges[9]=new Edge('A', 'E', 9);
 
+        for (int i = 0; i < p.length; i++) { //初始化并查集
+            p[i]=i;
+        }
+    }
+    public static void main(String[] args) {
+        int res= solve(5);
+        System.out.println(res);
+        System.out.println(list);
 
-    class Edge implements Comparable<Edge>{
-        int start;
-        int end;
+    }
+
+    /**
+     *
+     * @param n //总顶点数量
+     */
+    static int solve(int n){
+        //sort
+        Arrays.sort(edges);
+
+        int res=0; //最小生成树权重值
+
+        //开始遍历边集， 找出最小生成树的边集合list和最小权重，前驱
+        for (int i = 0; i < edges.length; i++) {
+            Edge edge = edges[i];
+            int a=find(edge.start);
+            int b=find(edge.end);
+            if (a!=b){ //两个顶点不在同一并查集（不在同一棵子树，不会形成回路）
+                p[b]=a; //将他们两个放入同一并查集
+                res+=edge.value;
+                prev[b]=a; //每个顶点的前驱
+                list.add(edge); //加入最小生成树
+                if (list.size()==n-1)
+                    break;
+            }
+        }
+        if (list.size()==n-1) //最后检查若是不同并查集的边少于n-1， 则该图不连通
+            return res;
+        else return -1;
+    }
+
+    static int find(int x){
+        if (p[x]!=x) p[x]=find(p[x]);
+        return p[x];
+    }
+
+    static class Edge implements Comparable<Edge>{
+        char start;
+        char end;
         int value;
 
-        public Edge(int start, int end, int value) {
+        public Edge(char start, char end, int value) {
             this.start = start;
             this.end = end;
             this.value = value;
@@ -26,6 +83,15 @@ public class Kruskal {
         @Override
         public int compareTo(Edge o) {
             return this.value-o.value;
+        }
+
+        @Override
+        public String toString() {
+            return "Edge{" +
+                    "start=" + start +
+                    ", end=" + end +
+                    ", value=" + value +
+                    '}';
         }
     }
 }
--- src/greedy_贪心/区间覆盖_B站.java
+++ src/greedy_贪心/区间覆盖_B站.java
@@ -28,49 +28,49 @@ import java.util.Scanner;
 
 // 区间覆盖: 最少, 排序按照start点越早的越前,依据也是上一个区间的end点
 /*
-  思路: 这次的重叠区间就变成了"重叠线段起点S"的了,要反过来,开始start点最早(小)的区间排在前面,
-        如果start相同则end小的排在前面;
-        遍历选出即能覆盖,距离又远的线段[s,t] .
-        若下一个区间不再重叠线段起点start_now,则更新线段起点S,转入下一段线段[s',t],重复以上操作直至区间没了或者end_now>=T
+  思路: 这次的重叠区间就变成了"重叠线段起点S"的了,要反过来,开始start点最早(小)的区间排在前面,如果start相同则end小的排在前面;
+        选出既能够连接上一个区间终点end_old, 又伸得最远的线段[s,t] .
+        若下一个区间不再重叠线段起点start_now,则更新线段起点S,转入下一段线段[s',t],重复以上操作直至遍历完线段或者end_now>=T
   解法:  1.创建Job类,实现Comparable接口,包含start和end两个类变量;
         2.所有区间放入一个Job[]数组中,对其进行排序(start小的在前,相同end小的在前);
             判断job[length-1].end是否能覆盖线段最远端
-        3.(贪心)保存历史终点的end_his,上一条重叠线段的终点end_last;
-        4.初始化end_now=S(要覆盖的起点),count=1,
+        3.(贪心)保存上一个区间的终点的end_old,上一条重叠线段的终点end_last;
+        4.初始化end_old=S(要覆盖的起点),end_last=jobs[0].end,count=1,
         5,若是当前线段的起点s不能覆盖上一条线段的终点end_last,那就永远不能覆盖完线段了,错误退出返回-1;
-            若是不断开,是区间能够覆盖起点S,那么若是当前区间end更大则更新end_now;
-            第三种情况是当前区间不重叠当前起点S了,就更新起点start_now=end_now,count++, 继续往后遍历;
-            最后就是T<=end_now结束, count++;
+          若是不断开,那么如果该区间还能连接前一个区间终点end_old,考虑更新end_last;
+                       如果是不连接了,就更新起点end_old=end_last,end_last=t(当前终点);count++, 继续往后遍历;
+          最后就是T<=end_now就跳出. 输出结果;
+
 * */
 public class 区间覆盖_B站 {
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        int N=in.nextInt();
-        int S=1;
-        int T=in.nextInt();
-        Job[] jobs=new Job[N];
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int S = 1;
+        int T = in.nextInt();
+        Job[] jobs = new Job[N];
         for (int i = 0; i < N; i++) {
-            jobs[i]=new Job(in.nextInt(),in.nextInt());
+            jobs[i] = new Job(in.nextInt(), in.nextInt());
         }
         /*step.2 对job类排序,判断最远点*/
         Arrays.sort(jobs);
         System.out.println(Arrays.toString(jobs));
-        if (jobs[jobs.length-1].end < T  //首或尾不能覆盖
-               || jobs[0].start> S){
-            System.out.println(-1+"aa");
+        if (jobs[jobs.length - 1].end < T  //首或尾不能覆盖
+                || jobs[0].start > S) {
+            System.out.println(-1 + "aa");
             System.exit(0);
         }
         /*step.3 初始化start_now点与end_now点,count=1*/
-        int end_his=S;
-        int end_last=S;
-        int cnt=1;
+        int end_his = S;
+        int end_last = S;
+        int cnt = 1;
         /*step.4 遍历排序好的区间, 若有点不重叠上一次end就交换起点和终点*/
         for (int i = 0; i < N; i++) {
-            int s=jobs[i].start;
-            int t=jobs[i].end;
+            int s = jobs[i].start;
+            int t = jobs[i].end;
 
             //第一种情况, 当前线段和上一条线段的终点end_last不相连, 出现断层，不能覆盖
-            if(s>end_last) {
+            if (s > end_last) {
                 System.exit(0);
             }
             //第二种情况,当前线段起点和上一条线段相连
@@ -80,40 +80,40 @@ public class 区间覆盖_B站 {
                     end_his = end_last;
                     end_last = t;
                     cnt++;//更新要覆盖的线段,计数加一
-                }
-                else //否则就判断是否更新上一条的终点
+                } else //否则就判断是否更新上一条的终点
                     end_last = Math.max(end_last, t);
             }
+
             if (end_last >= T) //完成覆盖,退出
                 break;
         }
         System.out.println(cnt);
     }
 
-}
+    /*step.1 封装, 创建job类*/
+    static class Job implements Comparable<Job> {
+        int start;
+        int end;
 
-/*step.1 封装, 创建job类*/
-class Job implements Comparable<Job>{
-    int start;
-    int end;
+        public Job(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
 
-    public Job(int start,int end){
-        this.start=start;
-        this.end=end;
-    }
+        public int compareTo(Job other) {
+            int x = this.start - other.start;
+            if (x == 0)
+                return this.end - other.end;
+            else return x;
+        }
 
-    public int compareTo(Job other){
-        int x=this.start- other.start;
-        if (x==0)
-            return this.end-other.end;
-        else return x;
-    }
+        @Override
+        public String toString() {
+            return "Job{" +
+                    "start=" + start +
+                    ", end=" + end +
+                    '}';
+        }
 
-    @Override
-    public String toString() {
-        return "Job{" +
-                "start=" + start +
-                ", end=" + end +
-                '}';
     }
 }
