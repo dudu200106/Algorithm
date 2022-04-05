6c8d0fa3ef9bc52a2e4fbb094069690ce1a6c199
dudu200106 <1247187273@qq.com>
2022-04-05 09:24:28 +0800

    Review

--- src/graph_图论/图的dfs/Kruskal.java
+++ src/graph_图论/图的dfs/Kruskal.java
@@ -1,9 +1,33 @@
 package graph_图论.图的dfs;
 
-import com.sun.javafx.geom.Edge;
+import java.util.ArrayList;
 
-import java.util.*;
-
+/* 最小生成树*/
 public class Kruskal {
 
+    public static Edge[] edges; //边集合
+    public static ArrayList<Edge> list; //存放最小生成树的边
+    public static int[] prev;  //每个顶点的最小权重前驱
+    public static int[] p; //并查集
+
+
+
+    class Edge implements Comparable<Edge>{
+        int start;
+        int end;
+        int value;
+
+        public Edge(int start, int end, int value) {
+            this.start = start;
+            this.end = end;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.value-o.value;
+        }
+    }
 }
+
+
