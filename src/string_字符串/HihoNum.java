44d8ac9923b9066b8c9e5dad01e3ec9bdb90c55c
dudu200106 <1247187273@qq.com>
2022-04-08 10:51:23 +0800

    Review

--- src/string_字符串/HihoNum.java
+++ src/string_字符串/HihoNum.java
@@ -1,6 +1,10 @@
 package string_字符串;
 
 public class HihoNum {
+    public static void main(String[] args) {
+        String str ="ho99ih000hooih";
+        System.out.println(solve(str));
+    }
 
     static String solve(String s) {
         String hiho="hiho";
@@ -10,53 +14,46 @@ public class HihoNum {
         int head=0;
         int row=0;
 
-        while (head<s.length()-4) {
+        while (head<=s.length()-4) {
             //头
             char ch =s.charAt(head);
-            if (hiho.indexOf(ch)>0) { //停下
-                if (head>=row && containAll(s,head,row)) { //注意头不要超过尾部
-                    if (row-head+1 < minLen) { //全部包含且更新了,那尾部继续往后也没有作用,
-                        start_res=head;
-                        end_res = row;
-                        minLen = row-head+1;
+            if (hiho.indexOf(ch)>=0) { //找到有用的字符,停下
+
+                if (row == 0) { //第一次移动首部,得将当前末尾提到前面来
+                    row = head;
+                }
+
+                //尾部移到符合判定的有用字符
+                while (row < s.length()) {
+                    char cr = s.charAt(row);
+                    if (hiho.indexOf(cr) >= 0) { //有用
+                        if (containAll(s, head, row)) { //符合判定
+                            if (check(s,head,row) && row - head + 1 < minLen) {
+                                minLen = row - head + 1;
+                                start_res = head;
+                                end_res = row;
+                            }
+                            break; //停下, 不用row++
+                        }
+                        else
+                            row++; //有用.但不符合判定继续后移尾部
                     }
-                    head++;
+                    else
+                        row++; //没有用
                 }
-
             }
-            else {
-                head++;
-            }
-
-
-            if (row==0) { //第一次的时候得将当前末尾提到头前
-                row = head +1;
-            }
-
-            //尾部
-            while (row<s.length()) {
-                char ch1= s.charAt(row);
-                if (hiho.indexOf(ch1)<0) {
-                    row++;
-                    continue;
-                }
-                else if (containAll(s, head, row)) {
-                    if (head - row +1 < minLen) {
-                        minLen =head-row +1;
-                        start_res =head;
-                        end_res =row;
-                    }
-                    break; //停下, 不用row++
-                }
-                row++;
-
-            }
+            head++;
 
         }
-        if (minLen==Integer.MAX_VALUE)
+
+        if (minLen==Integer.MAX_VALUE){
+            System.out.println(start_res + " " + end_res);
             return "";
-        else
-            return s.substring(start_res,end_res+1);
+        }
+        else {
+            System.out.println(start_res + " " + end_res);
+            return s.substring(start_res, end_res + 1);
+        }
 
     }
 
@@ -66,9 +63,25 @@ public class HihoNum {
         int o=0;
 
         for (int j = head; j <= row; j++) {
-            if (s.charAt(i)=='h') h++;
-            if (s.charAt(i)=='i') i++;
-            if (s.charAt(i)=='o') o++;
+            if (s.charAt(j)=='h') h++;
+            if (s.charAt(j)=='i') i++;
+            if (s.charAt(j)=='o') o++;
+        }
+        if (h>=2 && i>=1 && o>=1)
+            return true;
+        else
+            return false;
+    }
+
+    static boolean check(String s, int head, int row){
+        int h=0;
+        int i=0;
+        int o=0;
+
+        for (int j = head; j <= row; j++) {
+            if (s.charAt(j)=='h') h++;
+            if (s.charAt(j)=='i') i++;
+            if (s.charAt(j)=='o') o++;
         }
         if (h==2 && i==1 && o==1) {
             return true;
